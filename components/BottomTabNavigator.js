af17ae1a98e626826b58750074ff05e6dfd2cf60
byjusmx <87218747+byjusmx@users.noreply.github.com>
2021-09-19 00:50:31 -0500

    Update BottomTabNavigator.js

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -24,7 +24,7 @@ export default class BottomTabNavigator extends Component {
                 iconName = "search";
               }
 
-              // You can return any component that you like here!
+              // Puedes devolver cualquier componente que quieras aquí
               return (
                 <Ionicons
                   name={iconName}
@@ -60,8 +60,8 @@ export default class BottomTabNavigator extends Component {
             }
           }}
         >
-          <Tab.Screen name="Transaction" component={TransactionScreen} />
-          <Tab.Screen name="Search" component={SearchScreen} />
+          <Tab.Screen name="Transación" component={TransactionScreen} />
+          <Tab.Screen name="Buscar" component={SearchScreen} />
         </Tab.Navigator>
       </NavigationContainer>
     );
